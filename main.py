b5c7644a65b93d2ddbeca8d54285a48c437438e4
Raffael <rb@logentis.de>
2024-01-11 21:29:23 +0100

    intermidiate main state

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello, World!")
+import pandas as pd
 
+
+def main():
+    print("Start")
+    import_data(FILE_PATH)
+
+
+def import_data(file_path):
+    data = pd.read_csv(file_path, delimiter=',', nrows = 5000, engine='python', encoding='utf-8')
+
+
+FILE_PATH = "assets/blogtext.csv"
 
 # driver
 if __name__ == "__main__":
