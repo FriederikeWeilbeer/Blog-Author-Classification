5981247b5617f5b7fc16bac3108de5d3d78ed5f6
Raffael <rb@logentis.de>
2024-01-11 21:55:00 +0100

    intermediate main state 1.2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import csv
 import pandas as pd
+
 
 
 def main():
@@ -8,10 +8,12 @@ def main():
 
 
 def import_data(file_path):
-    data = pd.read_csv(file_path, delimiter=',', nrows=10000, engine='python', encoding='utf-8')
+    data = pd.read_csv(file_path, delimiter=',', nrows=COMPLETE_DATA_LENGTH, encoding='utf-8', on_bad_lines='skip')
     print(data)
 
+
 FILE_PATH = "assets/blogtext.csv"
+COMPLETE_DATA_LENGTH = 681284
 
 # driver
 if __name__ == "__main__":
