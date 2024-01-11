1bd9a6893a2d78f5bc3561102bf07d3ca8592423
Raffael <rb@logentis.de>
2024-01-11 22:41:26 +0100

    intermediate main state 1.3

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import pandas as pd
-
+import nltk
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+import time
+from tqdm import tqdm
 
 
 def main():
     print("Start")
-    import_data(FILE_PATH)
+    now = time.time()
+    data = import_data(FILE_PATH, 100000)
+    print(data.columns)
+    prepare_data(data)
+    print(time.time() - now)
 
 
-def import_data(file_path):
-    data = pd.read_csv(file_path, delimiter=',', nrows=COMPLETE_DATA_LENGTH, encoding='utf-8', on_bad_lines='skip')
-    print(data)
+def import_data(file_path, rows):
+    data_frame = pd.read_csv(file_path, delimiter=',', nrows=rows, encoding='utf-8', on_bad_lines='skip')
+    return data_frame
 
+
+def remove_stopwords(text):
+
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if w not in stop_words]
+
+    return ' '.join(tokens)
+
+
+def prepare_data(data_frame):
+    print(data_frame['text'].apply(len).mean())
+    data_frame['text'] = data_frame['text'].apply(remove_stopwords)
+    print(data_frame['text'].apply(len).mean())
 
 FILE_PATH = "assets/blogtext.csv"
 COMPLETE_DATA_LENGTH = 681284
