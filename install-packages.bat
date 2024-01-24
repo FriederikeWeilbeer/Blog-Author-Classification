1b99dd4af489461c633423e5ea661f775f65ab90
Friederike Weilbeer <friederike.weilbeer@gmail.com>
2024-01-24 15:02:51 +0100

    add batch file

---
+++ install-packages.bat
@@ -0,0 +1,23 @@
+@echo off
+
+REM Install scikit-learn
+pip install -U scikit-learn
+
+REM Install nltk and download stopwords
+pip install -U nltk
+python -m nltk.downloader stopwords
+
+REM Install numpy
+pip install -U numpy
+
+REM Install pandas
+pip install -U pandas
+
+REM Install seaborn
+pip install seaborn
+
+REM Install flask
+pip install flask
+
+REM Install scikit-learn-intelex (for Intel processors)
+pip install scikit-learn-intelex
