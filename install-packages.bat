73a919c05d5d04cbf68ff42968bfb828ca272ace
Friederike Weilbeer <friederike.weilbeer@gmail.com>
2024-01-24 15:27:12 +0100

    edit batch file

--- install-packages.bat
+++ install-packages.bat
@@ -1,23 +1,23 @@
 @echo off
 
 REM Install scikit-learn
-pip install -U scikit-learn
+!pip install -U scikit-learn
 
 REM Install nltk and download stopwords
-pip install -U nltk
-python -m nltk.downloader stopwords
+!pip install -U nltk
+!python -m nltk.downloader stopwords
 
 REM Install numpy
-pip install -U numpy
+!pip install -U numpy
 
 REM Install pandas
-pip install -U pandas
+!pip install -U pandas
 
 REM Install seaborn
-pip install seaborn
+!pip install seaborn
 
 REM Install flask
-pip install flask
+!pip install flask
 
 REM Install scikit-learn-intelex (for Intel processors)
-pip install scikit-learn-intelex
+!pip install scikit-learn-intelex
