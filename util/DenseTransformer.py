2d70fd8b01255cdbcf7b840c056f07016715354f
Raffael <rb@logentis.de>
2024-01-28 10:20:17 +0100

    Added DenseTransformer.py

---
+++ util/DenseTransformer.py
@@ -0,0 +1,10 @@
+# Custom transformer to convert sparse matrix to dense array
+from sklearn.base import TransformerMixin
+
+
+class DenseTransformer(TransformerMixin):
+    def transform(self, x, **transform_params):
+        return x.toarray()
+
+    def fit(self, x, y=None, **fit_params):
+        return self
